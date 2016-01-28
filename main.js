ba182ce5adfa083941780083b0e55710104df9df
Subham Chatterjee <subhamchatterjee@users.noreply.github.com>
2016-01-28 16:39:49 +0530

    Button animation added

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
+    // Text animation class toggle
     $(".spongy").mouseover(function () {
         $(this).toggleClass("sponge");
     });
     $(".elegantshadow").mouseover(function () {
         $(this).toggleClass("sponge2");
+    });
+    
+    // Button animation class toggle
+    $(".btn").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function () {
+        $(this).removeClass("btn_animate");  
+    });
+    $(".btn").hover(function () {
+        $(this).addClass("btn_animate");        
+    });
+    $(".btn2").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function () {
+        $(this).removeClass("btn_animate2");  
+    });
+    $(".btn2").hover(function () {
+        $(this).addClass("btn_animate2");        
     });
 });
