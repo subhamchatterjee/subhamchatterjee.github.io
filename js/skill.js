d34ebc05a178630deeba0c088d507e68ea687229
radiobeats <subham.b.chatterjee@gmail.com>
2016-02-29 02:27:38 +0530

    About and contact pages added

---
+++ js/skill.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".progressbars").jprogress({
+    });
+    $(".progressbar").jprogress({
+        background: '#64CA00'
+    });
+});
--- js/work.js
+++ js/work.js
@@ -1,3 +1,23 @@
+var SCREEN_WIDTH = window.innerWidth;
+var SCREEN_HEIGHT = window.innerHeight;
+var mob = false;
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    mob = true;
+}
+
+$(document).ready(function () {
+window.onload = deviceCheck();
+});
+
+function deviceCheck() {
+    if(mob == true) {
+        var mob_works = "<div class='mob_works'><h1>Please view the website in a larger screen.</h1><br/><a href='index.html'>Home</a></div>";
+        var content = document.getElementById("content_two");
+        content.innerHTML = mob_works;
+    }
+}
+
 function next(e) {
     if ($('.active').hasClass('one')) {
         $('html,body').animate({
@@ -51,3 +71,4 @@ function prev(e) {
         document.getElementById('prev').style.display = "none";
     }
 }
+
