8aa83f352327420d9f8f25dfa8363d145465f293
Subham Chatterjee <subhamchatterjee@users.noreply.github.com>
2016-04-15 18:42:36 +0530

    Update script.js

--- scoring_tool/script.js
+++ scoring_tool/script.js
@@ -1,23 +1,39 @@
-var seek1 = document.getElementById("q1");
-var seek2 = document.getElementById("q2");
-var seek3 = document.getElementById("v1");
-var seek4 = document.getElementById("v2");
-var seek5 = document.getElementById("v3");
-seek1.addEventListener("input",showValue);
-seek2.addEventListener("input",showValue);
-seek3.addEventListener("input",showValue);
-seek4.addEventListener("input",showValue);
-seek5.addEventListener("input",showValue);
-function showValue() {
-    document.getElementById("alg_scr").innerHTML = parseInt(seek1.value);
-    document.getElementById("arth_scr").innerHTML = parseInt(seek2.value);
-    document.getElementById("sc_scr").innerHTML = parseInt(seek3.value);
-    document.getElementById("cr_scr").innerHTML = parseInt(seek4.value);
-    document.getElementById("rc_scr").innerHTML = parseInt(seek5.value);
-    var q = parseInt(seek1.value) + parseInt(seek2.value);
-    var v = parseInt(seek3.value) + parseInt(seek4.value) + parseInt(seek5.value);
-    var t = q + v;
-    document.getElementById("q-value").innerHTML = q;
-    document.getElementById("v-value").innerHTML = v;
-    document.getElementById("t-value").innerHTML = t;
-}
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    window.addEventListener("load", showValue);
+    var q1Elem = document.getElementById("q1");
+    var q2Elem = document.getElementById("q2");
+    var v1Elem = document.getElementById("v1");
+    var v2Elem = document.getElementById("v2");
+    var v3Elem = document.getElementById("v3");
+
+    q1Elem.addEventListener("input", showValue);
+    q2Elem.addEventListener("input", showValue);
+    v1Elem.addEventListener("input", showValue);
+    v2Elem.addEventListener("input", showValue);
+    v3Elem.addEventListener("input", showValue);
+
+    function showValue() {
+        var q1 = parseInt(q1Elem.value);
+        var q2 = parseInt(q2Elem.value);
+
+        var v1 = parseInt(v1Elem.value);
+        var v2 = parseInt(v2Elem.value);
+        var v3 = parseInt(v3Elem.value);
+
+        document.getElementById("alg_scr").innerHTML = q1;
+        document.getElementById("arth_scr").innerHTML = q2;
+
+        document.getElementById("sc_scr").innerHTML = v1;
+        document.getElementById("cr_scr").innerHTML = v2;
+        document.getElementById("rc_scr").innerHTML = v3;
+
+        var q = q1 + q2;
+        var v = v1 + v2 + v3;
+        var t = q + v;
+
+        document.getElementById("q-value").innerHTML = q;
+        document.getElementById("v-value").innerHTML = v;
+        document.getElementById("t-value").innerHTML = t;
+    }
+});
