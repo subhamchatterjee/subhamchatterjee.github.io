63a9d85b4c916769ff7437c6fb701d8edfb58020
Subham Chatterjee <subham.b.chatterjee@gmail.com>
2016-08-18 18:50:45 +0530

    Updated Styles

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -3,18 +3,29 @@ $(document).ready(function() {
     var $navbar = $('.navbar');
     $navButton.click(function() {
         if($navbar.hasClass('active')) {
-            $navbar.removeClass('active');
-            $navButton.removeClass('getdown');
-            console.log("hiding navbar");
-            $('.navicon').removeClass('fa-times').addClass('fa-bars');
+            hideNavbar();
         }
         else {
-            $navbar.addClass('active');
-            $navButton.addClass('getdown');
-            $('.nav').css('background-color', '#303030');
-            $('.navicon').addClass('fa-times').removeClass('fa-bars');
+            showNavbar();
         }
     });
+    
+    
+    if($('.banner-image').height() == 600) {
+        console.log("boro chobi");
+        $('.banner').addClass('background');
+    }
+    else $('.banner').removeClass('background');
+    
+    $('.navlink').click(function() {
+        $('.navlink').each(function() {
+            $(this).removeClass('active');
+            console.log("removing active class");
+        });
+        $(this).addClass('active');
+        hideNavbar();
+    });
+    
     $('.type-heading').click(function() {
         if(!($(this).hasClass('active'))) {
             $('.type-heading').each(function() {
@@ -23,8 +34,7 @@ $(document).ready(function() {
             $(this).addClass('active');
             if($(this).hasClass('skills')) {
                 var SCREEN_WIDTH = $(window).width();
-                $('.container').addClass('animate');
-                $('.container').css('transform', 'translateX(-' + SCREEN_WIDTH + 'px)');
+                $('.container').addClass('animate').css('transform', 'translateX(-' + SCREEN_WIDTH + 'px)');
                 $('.skills-body').addClass('active');
                 $('.bio-body').removeClass('active');
                 $('.value').each(function(){
@@ -34,8 +44,7 @@ $(document).ready(function() {
                 });
             }
             else {
-                $('.container').addClass('animate');
-                $('.container').css('transform', 'translateX(0px)');
+                $('.container').addClass('animate').css('transform', 'translateX(0px)');
                 $('.skills-body').removeClass('active');
                 $('.bio-body').addClass('active');
             }
@@ -46,7 +55,24 @@ $(document).ready(function() {
 $(window).resize(function() {
     if($('.skills').hasClass('active')) {
         var SCREEN_WIDTH = $(window).width();
-        $('.container').removeClass('animate');
-        $('.container').css('transform', 'translateX(-' + SCREEN_WIDTH + 'px)');
+        $('.container').removeClass('animate').css('transform', 'translateX(-' + SCREEN_WIDTH + 'px)');
     }
+    if($('.banner-image').height() == 600) {
+        console.log("boro chobi");
+        $('.banner').addClass('background');
+    }
+    else $('.banner').removeClass('background');
 });
+
+function showNavbar() {
+    $('.navbar').addClass('active');
+    $('.nav-button').addClass('getdown');
+    $('.nav').css('background-color', '#303030');
+    $('.navicon').addClass('fa-times').removeClass('fa-bars');
+}
+
+function hideNavbar() {
+    $('.navbar').removeClass('active');
+    $('.nav-button').removeClass('getdown');
+    $('.navicon').removeClass('fa-times').addClass('fa-bars');
+}
